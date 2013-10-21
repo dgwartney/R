48d98f7d2a2a9788591d56e85e801f63a52e4d15
David Gwartney <dgwartney@zynga.com>
2013-10-20 20:13:13 -0700

    Homework completed

--- statistics-one/assignments/week4/stats1-supplemental-Stats1.13.Lab.04.R
+++ statistics-one/assignments/week4/stats1-supplemental-Stats1.13.Lab.04.R
@@ -25,11 +25,11 @@
 library(psych)
 
 # Read data into a dataframe called PE (physical endurance)
-PE <- read.table("Stats1.13.Lab.04.txt", header = T)
+PE <- read.table("Stats1.13.HW.04.txt", header = T)
 
 # If you want to view the data
 #View(PE)
-edit(PE)
+#edit(PE)
 
 # Summary statistics
 describe(PE) 
@@ -39,40 +39,46 @@ cor(PE[2:4]) # Omit column 1 because it contains participant id numbers
 
 round(cor(PE[2:4]), 2) # Round to 2 decimal places 
 
-cor.test(PE$age, PE$activeyears)
-cor.test(PE$endurance, PE$activeyears)
-cor.test(PE$endurance, PE$age)
+cor.test(PE$salary, PE$courses)
+cor.test(PE$salary, PE$years)
 
 # Histograms
 layout(matrix(c(1,2,3,4), 2, 2, byrow = TRUE))
-hist(PE$age)
-hist(PE$activeyears)
-hist(PE$endurance)
+hist(PE$courses)
+hist(PE$years)
+hist(PE$salary)
+layout(matrix(c(1,1), 1, 1, byrow = TRUE))
+layout(matrix(c(1,2,3,4), 2, 2, byrow = TRUE))
+boxplot(PE$courses)
+boxplot(PE$years)
+boxplot(PE$salary)
 layout(matrix(c(1,1), 1, 1, byrow = TRUE))
 
 
 # Regression analyses, unstandardized
-model1 <- lm(PE$endurance ~ PE$age)
+model1 <- lm(PE$salary ~ PE$years)
 summary(model1)
-plot(PE$endurance ~ PE$age, main = "Scatterplot", ylab = "Endurance", xlab = "Age")
-abline(lm(PE$endurance ~ PE$age), col="blue")
+plot(PE$salary ~ PE$years, main = "Scatterplot", ylab = "Salary", xlab = "Years")
+abline(lm(PE$salary ~ PE$years), col="blue")
 
-model2 <- lm(PE$endurance ~ PE$activeyears)
+model2 <- lm(PE$salary ~ PE$courses)
 summary(model2)
-plot(PE$endurance ~ PE$activeyears, main = "Scatterplot", ylab = "Endurance", xlab = "Active Years")
-abline(lm(PE$endurance ~ PE$activeyears), col="blue")
+plot(PE$salary ~ PE$courses, main = "Scatterplot", ylab = "Salary", xlab = "Courses")
+abline(lm(PE$salary ~ PE$years), col="blue")
 
-model3 <- lm(PE$endurance ~ PE$age + PE$activeyears)
+model3 <- lm(PE$salary ~ PE$years + PE$courses)
 summary(model3)
 
 # To visualize model3, save the predicted scores as a new variable and then plot with endurance
 PE$predicted <- fitted(model3)
+mean(PE$predicted)
 
-plot(PE$endurance ~ PE$predicted, main = "Scatterplot", ylab = "Endurance", xlab = "Model 3 Predicted Scores")
-abline(lm(PE$endurance ~ PE$predicted), col="blue")
+plot(PE$salary ~ PE$predicted, main = "Scatterplot", ylab = "Salary", xlab = "Model 3 Predicted Scores")
+abline(lm(PE$salary ~ PE$predicted), col="blue")
 
 # The function fitted returns predicted scores whereas the function resid returns residuals
 PE$e <- resid(model3)
+mean(PE$e)
 
 hist(PE$e)
 plot(PE$predicted ~ PE$e, main = "Scatterplot", ylab = "Model 3 Predicted Scores", xlab = "Model 3 Residuals")
@@ -83,12 +89,12 @@ abline(lm(PE$predicted ~ PE$e), col="blue")
 
 round(cor(PE[2:4]), 2) # Round to 2 decimal places 
 
-model1.z <- lm(scale(PE$endurance) ~ scale(PE$age))
+model1.z <- lm(scale(PE$salary) ~ scale(PE$years))
 summary(model1.z)
 
-model2.z <- lm(scale(PE$endurance) ~ scale(PE$activeyears))
+model2.z <- lm(scale(PE$salary) ~ scale(PE$courses))
 summary(model2.z)
 
-model3.z <- lm(scale(PE$endurance) ~ scale(PE$age) + scale(PE$activeyears))
+model3.z <- lm(scale(PE$salary) ~ scale(PE$courses) + scale(PE$years))
 summary(model3.z)
 
